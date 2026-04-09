5a0706da4b39ef36d6c001e94a41ccf368ec26a9
muhadil1 <muh.adil33.ma19@gmail.com>
2026-04-09 13:23:12 +0200

    CORS restritivo

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT;
 
 // --- MIDDLEWARES ---
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Parse de form data
 
