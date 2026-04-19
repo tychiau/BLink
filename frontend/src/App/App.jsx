2c472628243fe9668c07201469f92544441ce6b7
Elton <mesquita2019e.l.t.o.n@gmail.com>
2026-04-19 21:47:13 +0200

    atualização de rota

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -2,15 +2,25 @@ import { Routes, Route } from 'react-router-dom';
 import AuthPage from '../pages/Auth/AuthPage';
 import DashboardVendedor from '../pages/Vendedor/DashboardVendedor';
 import ClienteDashboardPage from '../pages/Cliente/ClienteDashboardPage';
+import DashboardIntermediario from '../pages/Intermediario/DashboardIntermediario';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
       <Route path="/auth" element={<AuthPage />} />
+
+      {/* VENDEDOR */}
       <Route path="/cadastro-produto" element={<DashboardVendedor />} />
       <Route path="/vendedor/dashboard" element={<DashboardVendedor />} />
+
+      {/* CLIENTE */}
       <Route path="/cliente/dashboard" element={<ClienteDashboardPage />} />
+
+      {/* INTERMEDIÁRIO */}
+      <Route path="/intermediario/dashboard" element={<DashboardIntermediario />} />
+
+      {/* FALLBACK */}
       <Route path="*" element={<div>Rota não encontrada</div>} />
     </Routes>
   );
